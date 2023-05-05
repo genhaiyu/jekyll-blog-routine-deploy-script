1d0c7bc0603dda222e8b55c65bbf3a7a67030154
Genhai Yu <gyu@genhaiyu.com>
2023-05-04 19:56:10 -0700

    Rewrite histories failed

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
-# jekyll-blog-routine-deploy-script
+# Automatically deploy and release a jekyll blog
 
-> Streamlining the steps of deploy a jekyll blog requires manual input of many commands after cutting off nginx traffic and processes
+[![GNU General Public License v3.0](https://img.shields.io/github/license/genhaiyu/jekyll-blog-routine-deploy-script)](https://github.com/genhaiyu/jekyll-blog-routine-deploy-script/blob/master/LICENSE)
+[![](https://img.shields.io/github/actions/workflow/status/genhaiyu/jekyll-blog-routine-deploy-script/check-build.yml)](https://github.com/genhaiyu/jekyll-blog-routine-deploy-script/blob/master/.github/workflows/check-build.yml)
 
+> Streamlining the steps of deploy a jekyll blog requires manual input of many commands after cutting off nginx traffic and processes.
+
+<img src="https://user-images.githubusercontent.com/17850202/222082624-4703c0bb-4812-4954-becf-90b67453e6d6.gif" width="500" alt="example"/>
 
 ## Documentation
 
-Download the script in blog directory where deployed on Linux, make sure the dir should have the `Gemfile`, which mean it is in the correct directory. 
+Download the script in blog directory where deployed on Linux, make sure the directory should have the `Gemfile`, which mean it is in the correct directory. This [A sample of Jekyll blog](https://github.com/genhaiyu/jekyll-example) passed built test, can be release a blog quickly.
 ```markdown
-wget -N --no-check-certificate -q -O deploy.sh "https://raw.githubusercontent.com/genhaiyu/jekyll-blog-routine-deploy-script/master/deploy.sh" && chmod +x deploy.sh && bash deploy.sh
+curl -sSLO https://raw.githubusercontent.com/genhaiyu/jekyll-blog-routine-deploy-script/master/deploy.sh && chmod a+x deploy.sh && bash deploy.sh
 ```
 
-The script automatically installs `rvm`, `ruby`, `nginx` these dependencies on the new server, otherwise it will only be updated routinely.
+The script automatically installs `rvm`, `ruby`, `nginx` these dependencies on the new server, otherwise it will only be updated, built, deploy, released routinely.
 
-## License
+Currently supported Linux systems: `Ubuntu 20.04 LTS x64`, `CentOS 8 Stream x64`, `CentOS 7 x64`
 
-[![GNU General Public License v3.0](https://img.shields.io/github/license/genhaiyu/jekyll-blog-routine-deploy-script)](https://github.com/genhaiyu/jekyll-blog-routine-deploy-script/blob/master/LICENSE)
+## Issues
+
+This script convenient for my routine deployment of `jekyll` blog.
--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 RED='\033[0;31m'
 NC='\033[0m'
-Orange='\033[0;33m'
+# Orange='\033[0;33m'
 Blue='\033[0;34m'
 Green='\033[0;32m'
 
@@ -13,21 +13,32 @@ abort() {
   exit 1
 }
 
-RV="3.1.0"
-T="dnf"
+DEFAULT_STABLE_VERSION="3.1.0"
+INSTALL_TYPE="dnf"
+# Base/Test version
+UV="20.04"
+# https://rvm.io/rvm/security#install-our-keys
+RVM_KEYS="--recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
 
 source '/etc/os-release'
 
+change_keys() {
+  GPG2="gpg2 --keyserver keys.openpgp.org $RVM_KEYS"
+}
+
+compare_version() {
+  perl -e "{if($1>=$2){print 1} else {print 0}}"
+}
+
 check_sys() {
   if [[ "${ID}" = "centos" && "${VERSION_ID}" == 8 ]]; then
-    V="C8"
+    change_keys
   elif [[ "${ID}" = "centos" && "${VERSION_ID}" == 7 ]]; then
-    # https://rvm.io/rvm/security#install-our-keys
-    GPG="gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
-    V="C7"
-    T="yum"
-    #    elif [[ "${ID}" = "ubuntu"  ]]; then
-
+    GPG="gpg --keyserver hkp://keyserver.ubuntu.com:80 $RVM_KEYS"
+    INSTALL_TYPE="yum"
+  elif [[ "${ID}" = "ubuntu" ]] && [[ "$(compare_version "${VERSION_ID}" $UV)" == 1 ]]; then
+    INSTALL_TYPE="apt-get"
+    change_keys
   else
     abort "The script doesn't support the current system!"
   fi
@@ -41,7 +52,7 @@ reload_bundle() {
   if [[ -d "./_site" ]]; then
     bundle clean --force
   fi
-  rvm use $RV
+  rvm use $DEFAULT_STABLE_VERSION
   bundle install
 }
 
@@ -54,6 +65,7 @@ check_dir() {
 
 # Private repositories, username and password required
 # git clone https://github.com/genhaiyu/genhaiyu.github.io.git
+# My blog, but redirected to a new site, https://genhaiyu.github.io
 check_repository_status() {
   read -rp "Do you want to get git repository up to date (y/n)? " update
   case "$update" in
@@ -68,40 +80,47 @@ check_repository_status() {
 }
 
 check_rvm_env() {
-  check_sys
   check_dir
+  check_sys
 
   if ! [[ -f "/usr/local/rvm/bin/rvm" ]]; then
-    if [ "$V" = "C7" ]; then
-      $GPG
+    if [[ "$INSTALL_TYPE" = "dnf" || "$INSTALL_TYPE" = "apt-get" ]]; then
+      $GPG2
     else
-      gpg2 --keyserver keys.openpgp.org --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+      $GPG
     fi
     curl -sSL https://get.rvm.io | bash -s stable
   fi
   reload_source
-
-  if ! [[ -f "/usr/bin/ruby" ]]; then
+  # Default in /usr/bin/ruby
+  if ! [[ -f "/usr/local/rvm/rubies/ruby-$DEFAULT_STABLE_VERSION/bin/ruby" ]]; then
     # Depends on gems or version compatibility in Gemfile
-    echo "Start installing ruby, it will take a few minutes."
-    rvm install $RV
+    echo "Start installing ruby, it may take a few minutes."
+    rvm install $DEFAULT_STABLE_VERSION
     sleep 3
   fi
 
-  JL="/usr/local/rvm/gems/ruby-$RV/bin/jekyll"
-  if ! [[ -f "$JL" ]]; then
+  jekyll_location="/usr/local/rvm/gems/ruby-$DEFAULT_STABLE_VERSION/bin/jekyll"
+  if ! [[ -f "$jekyll_location" ]]; then
     gem install jekyll bundler
   fi
   reload_bundle
 }
 
 build_posted() {
-  if [[ "${ID}" = "centos" ]]; then
+  if [[ $INSTALL_TYPE = "yum" ]] || [[ $INSTALL_TYPE = "dnf" ]]; then
     rm -rf /usr/share/nginx/html/*
     mv "_site"/* "/usr/share/nginx/html/"
-    chcon -Rt httpd_sys_content_t "/usr/share/nginx/html/"
-    sudo systemctl start nginx
+    if [[ $INSTALL_TYPE = 'dnf' ]]; then
+      chcon -Rt httpd_sys_content_t "/usr/share/nginx/html/"
+    fi
+  elif [[ $INSTALL_TYPE = "apt-get" ]]; then
+    rm -rf /var/www/html/*
+    mv "_site"/* "/var/www/html/"
+  else
+    abort "The script doesn't support the current system!"
   fi
+  sudo systemctl start nginx
 }
 
 deploy_posted() {
@@ -113,10 +132,14 @@ check_nginx() {
   if [[ -f "/usr/sbin/nginx" ]]; then
     echo "It is detected that nginx has been installed, skip it."
   else
-    if [ $V = "C7" ]; then
-      sudo $T install nginx
+    if [ $INSTALL_TYPE = "yum" ]; then
+      sudo $INSTALL_TYPE install nginx
+    elif [ $INSTALL_TYPE = "apt-get" ]; then
+      sudo $INSTALL_TYPE install nginx
+      sudo $INSTALL_TYPE install firewalld
     else
-      sudo $T install nginx
+      # dnf
+      sudo $INSTALL_TYPE install nginx
     fi
     sleep 3
     sudo systemctl enable nginx
